3faa1aeb12362b61dd499b25808f3d6201e9ea33
Maarten <maartenwachtersmast@gmail.com>
2017-09-30 11:30:17 +0200

    Uploading current progress of mcmovie : les 2 slide 14

---
+++ MvcMovie/Data/DBInitializer.cs
@@ -0,0 +1,28 @@
+﻿using MvcMovie.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcMovie.Data
+{
+    public class DBInitializer
+    {
+        public static void Initialize(MovieContext context)
+        {
+            context.Database.EnsureCreated();
+
+            //FL movies
+            if (context.Movies.Any())
+            {
+                return;
+            }
+
+            context.Ratings.AddRange(
+                new Rating { Code = "G", Name = "General" },
+                new Rating { Code = "PG", Name = "Parental Guidance" },
+                new Rating { Code = "M", Name = "Mature" });
+            context.SaveChanges();
+        }
+    }
+}
---
+++ MvcMovie/Data/MovieContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.Models;
+
+namespace MvcMovie.Data
+{
+    public class MovieContext : DbContext
+    {
+        public MovieContext(DbContextOptions<MovieContext> options) : base(options)
+        {
+        }
+
+            public DbSet<Movie> Movies { get; set; }
+            public DbSet<Rating> Ratings { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Movie>().ToTable("Movie");
+            modelBuilder.Entity<Rating>().ToTable("Rating");
+        }
+        
+    }
+}
